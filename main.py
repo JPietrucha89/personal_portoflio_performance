b78942734880c1c5869e2703e5d08521d890b249
Jakub Pietrucha <elpietruch@gmail.com>
2025-11-22 21:45:27 +0100

    feat: add try block and parametrized query that is not susceptible to SQL injection

--- main.py
+++ main.py
@@ -6,18 +6,24 @@ connection = sqlite3.connect('portfolio.db')
 
 cursor = connection.cursor()
 
-# Create starting table
-sql_string = '''
+# # Create starting table
+sql_string = """
 CREATE TABLE IF NOT EXISTS
-stores(store_id INTEGER PRIMARY KEY, location TEXT)
-'''
+stores(
+    store_id INTEGER PRIMARY KEY,
+    location TEXT)
+"""
 
 cursor.execute(sql_string)
 
-# add 3 rows
-cursor.execute("INSERT INTO stores VALUES(1, 'Warszawa')")
-cursor.execute("INSERT INTO stores VALUES(2, 'Kraków')")
-cursor.execute("INSERT INTO stores VALUES(3, 'Radom')")
+# add 3 rows if they don't already exist
+try:
+    cursor.execute("INSERT INTO stores VALUES(1, 'Warszawa')")
+    cursor.execute("INSERT INTO stores VALUES(2, 'Kraków')")
+    cursor.execute("INSERT INTO stores VALUES(3, 'Radom')")
+    connection.commit()
+except:
+    print("Rows already exist in 'stores' table")
 
 # get results
 sql_string = '''
@@ -30,3 +36,13 @@ print(results)
 
 for result in results:
     print(result)
+
+# get results while passing some variables or arguments
+print('get results while passing some variables or arguments')
+cursor.execute('select * from stores where store_id = :store_id or location = :location', {'store_id' : 2, 'location' : 'Radom'})
+results = cursor.fetchall()
+
+print(results)
+
+
+connection.close()
